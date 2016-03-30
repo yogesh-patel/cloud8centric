b768cc58b8fce0321ccd51af12b7c35bdb5d85e1
kalyani <kalyani.bagale@synerzip.com>
2016-03-30 14:53:37 +0530

    changed backend API endpoint to local

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 let config ={
-    BASE_URL: "https://ec2-54-255-134-192.ap-southeast-1.compute.amazonaws.com:8888/api/v1/",
-    LOGIN_BASE_URL: "https://ec2-54-255-134-192.ap-southeast-1.compute.amazonaws.com:8888/",
+    BASE_URL: "https://localhost:8888/api/v1/",
+    LOGIN_BASE_URL: "https://localhost:8888/",
     CLIENT_ID:"mobile",
     CLIENT_SECRETE:"centric8",
     ENV: 'DEV'
